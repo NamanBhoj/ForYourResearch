7d550806ef6dd3d526bf838204bd9eec9a366d22
Naman Bhoj <39219774+NamanBhoj@users.noreply.github.com>
2024-07-28 20:14:56 -0600

    resolving errors

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import FeatureSection from '../components/FeatureSection';
 import Footer from '../components/Footer';
 import MainSection from '../components/MainSection';
