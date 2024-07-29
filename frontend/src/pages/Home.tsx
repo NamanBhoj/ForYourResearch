bd4df5a5fa2e4557ce2d013d2a75be093d291e83
Naman Bhoj <39219774+NamanBhoj@users.noreply.github.com>
2024-07-28 20:17:59 -0600

    resolving lint fucking errors again

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import FeatureSection from '../components/FeatureSection';
 import Footer from '../components/Footer';
 import MainSection from '../components/MainSection';
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Signup from "../pages/SignUp";
 import Login from "../pages/Login"
 import Home from "../pages/Home";
-import React from "react";
+
 
 const routes = createBrowserRouter([
     { path: "/", element: <Home></Home> },
