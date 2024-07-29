f61fa5f8bea99500d07e2eaa29ddcfe9f04e239a
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-07-28 21:09:39 -0600

    Put router in App and put App in main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import Home from './pages/Home';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes';
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App.tsx';
-import routes from './routes/routes.tsx';
-import { RouterProvider } from 'react-router-dom';
-
-// import Home from './pages/Home.tsx';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <App />
   </React.StrictMode>
 );
