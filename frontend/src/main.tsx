0cc15320503a881c263d4e4cd0d94e6b507713f0
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-07-28 20:19:19 -0600

    Firebase and login/signup setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App.tsx';
-import Home from './pages/Home.tsx';
+import App from './App.tsx';
+import Signup from './pages/SignUp.tsx';
+import Login from './pages/Login.tsx';
 
-
+// import Home from './pages/Home.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home></Home>
+    <Login />
   </React.StrictMode>
 );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,6 @@ import MainSection from '../components/MainSection';
 import Navbar from '../components/Navbar';
 // import SignUp from '../pages/SignUp'
 // import Login from '../pages/Login'
-// import GetEarlyAccess from "../pages/GetEarlyAccess"
 function Home() {
   return (
     <div>
