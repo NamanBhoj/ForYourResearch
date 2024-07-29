7bde99019c45cddd9c019ab8f3cbed8b6f41f5c0
Naman Bhoj <39219774+NamanBhoj@users.noreply.github.com>
2024-07-28 20:33:50 -0600

    added missing router provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App.tsx';
-import Home from './pages/Home.tsx';
+import routes from './routes/routes.tsx';
+import { RouterProvider } from 'react-router-dom';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home></Home>
+     <RouterProvider router = {routes}></RouterProvider>
   </React.StrictMode>
 );
