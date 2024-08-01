d0b331750818492666bfe5fe02b2bc6d2313a30e
Naman Bhoj <namanbhoj99@gmail.com>
2024-08-01 10:11:57 -0600

    homepage changes

--- frontend/src/components/FeatureSection.tsx
+++ frontend/src/components/FeatureSection.tsx
@@ -23,7 +23,7 @@ function FeatureSection() {
 
   return (
     <div className="flex flex-col items-center mt-14 mb-8">
-      <span className="mt-24 text-5xl font-extrabold text-slate-900">
+      <span className="mt-24 text-5xl font-extrabold text-slate-900 animate">
         What we do
       </span>
       <img src={Arrow} alt="" className='animate-pulse' />
--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -3,14 +3,14 @@
 function MainSection() {
   return (
     <>
-      <h1 className="text-7xl font-extrabold text-slate-900 mb-4">
-        Automating tedious parts of literature reviews
+      <h1 className="text-7xl  font-extrabold text-slate-900 mb-4">
+        Automating tedious parts of literature reviews For Your Research
       </h1>
-      <p className="text-lg text-slate-700 mb-8 mt-2">
-        Streamline your research process with our analysis, classification, and
-        summarization tools, saving you time and ensuring accuracy in your
-        literature review.
+      <p className="text-lg font-bold  text-slate-700 mb-8 mt-2">
+        Save Time, Effort and Avoid Frustration while conducting Literature Reviews 
+        
       </p>
+     
       <a href="https://app.youform.com/forms/pvtbohr1" target="_blank">
         <button
           type="button"
