9b3c7df537bbacbc05ae5fbb6624a2d2f1666d1f
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-08-01 16:29:46 -0600

    Fix warning

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -181,6 +181,7 @@ export default function Signup() {
 
           <div>
             <button
+            disabled={pending}
               type="submit"
               className="flex w-full justify-center rounded-md bg-slate-950 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
