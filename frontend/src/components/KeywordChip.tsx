2bade691057c42c0ec978436b6909117da14f63c
Naman Bhoj <namanbhoj99@gmail.com>
2024-08-16 09:21:17 -0600

    added color changes to button

--- frontend/src/components/KeywordChip.tsx
+++ frontend/src/components/KeywordChip.tsx
@@ -4,12 +4,12 @@ function KeywordChip(props: any) {
       {props.text}
       <button
         type="button"
-        className="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-green-600/20"
+        className="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-red-200"
       >
         <span className="sr-only">Remove</span>
         <svg
           viewBox="0 0 14 14"
-          className="h-3.5 w-3.5 stroke-green-700/50 group-hover:stroke-green-700/75"
+          className="h-3.5 w-3.5 stroke-red-700 group-hover:stroke-red-700"
         >
           <path d="M4 4l6 6m0-6l-6 6" />
         </svg>
