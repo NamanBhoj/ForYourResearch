141482d27ed3b3f45b8c04b60c1f15249835a09b
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-08-18 22:24:46 -0600

    Fix warning

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,5 @@
 import Sidebar from '../components/Sidebar';
 import PapersTable from './PapersTable';
-import Library from './Library';
 
 function Dashboard() {
   return (
@@ -8,8 +7,8 @@ function Dashboard() {
       <Sidebar />
       <main className="py-10 lg:pl-72">
         <div className="px-4 sm:px-6 lg:px-8">
-          {/* <PapersTable /> */}
-          <Library />
+          <PapersTable />
+          {/* <Library /> */}
         </div>
       </main>
     </>
