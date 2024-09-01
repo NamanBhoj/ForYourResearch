fab423092ced9e2cbc4d25ba67fdc64b16b26919
Naman Bhoj <39219774+NamanBhoj@users.noreply.github.com>
2024-09-01 15:33:01 -0600

    Update MainSection.jsx

--- frontend/src/components/MainSection.jsx
+++ frontend/src/components/MainSection.jsx
@@ -1,4 +1,4 @@
-import BookingCard from './BookingCard';
+// import BookingCard from './BookingCard';
 function MainSection() {
   const handleScroll = () => {
     const section = document.getElementById('what-we-do');
@@ -10,7 +10,7 @@ function MainSection() {
   return (
     <>
       <div className="!scroll-smooth">
-        <BookingCard></BookingCard>
+{/*         <BookingCard></BookingCard> */}
         <div className="text-center mb-14 mt-12">
           <h1 className="text-7xl font-bold text-neutral-900 mb-4">
             Automating tedious parts of literature reviews
