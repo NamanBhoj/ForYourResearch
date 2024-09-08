c6a1622052a24f09a257ce82633bfce36a05204f
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-07 21:49:03 -0600

    Merge omar's update relevance branch to dev

--- frontend/src/components/Dashboard/Library.jsx
+++ frontend/src/components/Dashboard/Library.jsx
@@ -10,7 +10,10 @@ import { XCircleIcon } from '@heroicons/react/16/solid';
 
 export default function Library() {
   // const { user, signOut } = useUserAuth();
-  const lambdaUrl = 'http://127.0.0.1:8000';
+  const lambdaUrl =
+    'https://cnycft3yloelqv7wobyjbwahsy0ofgpy.lambda-url.us-east-2.on.aws';
+
+  // const lambdaUrl = 'http://127.0.0.1:8000';
 
   const [loading, setLoading] = useState(false);
 
@@ -69,7 +72,7 @@ export default function Library() {
 
     clonedPaperObj[selectedQuery] = updatedPapers;
     setPaperObj(clonedPaperObj);
-    setPapersToDisplay(clonedPaperObj[selectedQuery])
+    setPapersToDisplay(clonedPaperObj[selectedQuery]);
     // console.log(clonedPaperObj[selectedQuery]);
   };
 
