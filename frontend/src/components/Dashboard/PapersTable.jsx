346b3a6da2f0e9a9dee3e1b8b93b61335ff2f21b
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-07 22:18:23 -0600

    Update PapersTable.jsx

--- frontend/src/components/Dashboard/PapersTable.jsx
+++ frontend/src/components/Dashboard/PapersTable.jsx
@@ -16,7 +16,7 @@ export default function Library() {
   // const lambdaUrl =
   //   'https://cnycft3yloelqv7wobyjbwahsy0ofgpy.lambda-url.us-east-2.on.aws/';
   const lambdaUrl =
-    'https://hs4drk3vhdodm66fknnytg4biy0tnjdi.lambda-url.us-east-2.on.aws';
+    'https://hs4drk3vhdodm66fknnytg4biy0tnjdi.lambda-url.us-east-2.on.aws/';
   const [keyword, setKeyword] = useState('');
   // const [keywordList, setKeywordList] = useState([]);
   const [query, setQuery] = useState('');
