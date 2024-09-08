2d2d1d047b2509a2215c9b55d07f4011b7071489
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-08 13:28:30 -0600

    fix url

--- frontend/src/components/Dashboard/PapersTable.jsx
+++ frontend/src/components/Dashboard/PapersTable.jsx
@@ -13,7 +13,7 @@ import { XMarkIcon } from '@heroicons/react/20/solid';
 
 export default function Library() {
   const lambdaUrl =
-    'https://cnycft3yloelqv7wobyjbwahsy0ofgpy.lambda-url.us-east-2.on.aws/';
+    'https://cnycft3yloelqv7wobyjbwahsy0ofgpy.lambda-url.us-east-2.on.aws';
 
   const [keyword, setKeyword] = useState('');
   // const [keywordList, setKeywordList] = useState([]);
