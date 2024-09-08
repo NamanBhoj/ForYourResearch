219a9d10d2dc8f350220711a74efc62586a71e92
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-08 16:03:28 -0600

    Add query info page

--- frontend/src/components/Dashboard/PapersTable.jsx
+++ frontend/src/components/Dashboard/PapersTable.jsx
@@ -194,6 +194,7 @@ export default function Library() {
                       rows="2"
                     ></textarea> */}
                   </div>
+                  
                   <button
                     type="button"
                     className="inline-flex items-center justify-center rounded-md bg-green-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 mt-auto"
