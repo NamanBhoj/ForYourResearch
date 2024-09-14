af7255dca54050b0466e418911aeb78ee031bfe9
Naman Bhoj <namanbhoj99@gmail.com>
2024-09-14 15:34:10 -0600

    library operation for updating only manual relevance created

--- app/crud/library_operations.py
+++ app/crud/library_operations.py
@@ -55,6 +55,19 @@ def retrieve_papers_by_query(db:Session,search_id : int) :
     return (db.query(SearchResultSchema).filter(SearchResultSchema.search_id == search_id).all())
 
 
+def update_manual_paper_relevance(db:Session, search_id :int, title: str, relevance_value:str):
+    #functions gets a specific query for a specific user by using search id and then helps change the manual relevance
+
+    #search id maps: user and searchquery
+    #title : maps to specific paper
+    #manualrelevancevalue : relevance value send from frontend
+
+    paper = db.query(SearchResultSchema).filter(and_(SearchResultSchema.search_id == search_id , SearchResultSchema.title == title)).first()
+    paper.manual_overall_relevance = relevance_value
+    db.commit()
+    # db.refresh(paper)
+    # print(paper.manual_overall_relevance)
+
 
 
 
--- app/crud/testdb.py
+++ app/crud/testdb.py
@@ -1,34 +1,42 @@
-# from sqlalchemy import create_engine, and_
-# from sqlalchemy.orm import sessionmaker
-# from ..schemas.search import Search as SearchSchema 
-# from ..schemas.search_result import SearchResult as SearchResultSchema# Import your model
+from sqlalchemy import create_engine, and_
+from sqlalchemy.orm import sessionmaker
+from ..schemas.search import Search as SearchSchema 
+from ..schemas.search_result import SearchResult as SearchResultSchema# Import your model
 
-# # Set up the database connection
-# engine = create_engine("postgresql://doadmin:AVNS_nq_7NWCBc9D5HIhveK_@db-postgresql-tor1-74366-do-user-17707540-0.h.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
+# Set up the database connection
+engine = create_engine("postgresql://doadmin:AVNS_nq_7NWCBc9D5HIhveK_@db-postgresql-tor1-74366-do-user-17707540-0.h.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
  
-# Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# session = Session()
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = Session()
 
-# # Example query
+# Example query
 
 
 
-# def find_search_id(db:Session, uid: str , search_query: str): 
-#     "method to find search id for retreive_papers_by_query method"
+# def update_manual_paper_relevance(db:Session, search_id :int, title: str, relevance_value:str):
+#     #functions gets a specific query for a specific user by using search id and then helps change the manual relevance
 
-#     response = (db.query(SearchSchema.search_id).filter(and_(SearchSchema.uid == uid, SearchSchema.search_query == search_query)).all())
-#     # print(respone)
-#     a = response[0][0]
-#     return a
+#     #search id maps: user and searchquery
+#     #title : maps to specific paper
+#     #manualrelevancevalue : relevance value send from frontend
 
-# result = (find_search_id(session, "kvCvOJoraSbKkAobm8KD8VUnR1k1", '("virtual reality" OR "mixed reality" OR "augmented reality" OR "extended reality") AND "digital twin"'))
-# print(result)
-# # i = 0 
-# # for r in result:
-# #     i+=1
-# #     print(f"SEARCH_ID : {r.search_id}, title: {r.title} ,  abstract: {r.abstract} , year {r.year}")
+#     paper = db.query(SearchResultSchema).filter(and_(SearchResultSchema.search_id == search_id , SearchResultSchema.title == title)).first()
+#     paper.manual_overall_relevance = relevance_value
+#     db.commit()
+#     db.refresh(paper)
+#     print(paper.manual_overall_relevance)
 
-# # print(i)
+
+
+
+(update_manual_paper_relevance(session, 31, "Passive Haptic Feedback for Control Elements in Virtual Reality", "Relevant"))
+
+# i = 0 
+# for r in result:
+#     i+=1
+#     print(f"SEARCH_ID : {r.search_id}, title: {r.title} ,  abstract: {r.abstract} , year {r.year}")
+
+# print(i)
 
 
 
