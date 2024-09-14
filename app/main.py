b387f7a20d5ad8f0a621ea95a7dd09e49888fd10
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-14 16:55:37 -0600

    Fix middleware for aWS

--- app/main.py
+++ app/main.py
@@ -10,21 +10,21 @@ from .schemas.search import Search
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-# need allow origins for local only else it gives error in aws
-# if not (is_in_production()):
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=(["*"]),
 )
+# need allow origins for local only else it gives error in aws
+if not (is_in_production()):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_origins=(["*"]),
+    )
 
 app.include_router(papers.router)
 app.include_router(library.router)
