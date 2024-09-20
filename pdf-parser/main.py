bb6d396f9a10f7ae52d9a7061233406f8d6d56d3
Naman Bhoj <namanbhoj99@gmail.com>
2024-09-20 17:33:26 -0600

    s3 uplaod functionality

--- pdf-parser/main.py
+++ pdf-parser/main.py
@@ -2,16 +2,31 @@ import os
 import requests
 import json
 import certifi
+import boto3
+
+# Set AWS credentials as environment variables (optional if using aws configure)
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIASFUIRGOEJEK464CL'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-2'
+
+# Bucket name
+bucket_name = "paper-full-texts"
+
+# User UID folder
+s3_folder = 'some-user/'
+
+# Search string folder
+search_string = 'epidemic/'
+
+s3 = boto3.client('s3')
 
 # Open and read the JSON file
 with open("papers.json", "r") as file:
     data = json.load(file)
-    # print(data)
 
 def download_pdf_file(papers: list) -> bool:
-  
     res = []
-    #Connection Pooling so that the same connection can be used for multiple request making it a bit faster
+    # Connection Pooling so that the same connection can be used for multiple requests making it a bit faster
     session = requests.Session()
     session.verify = certifi.where()  # Use certifi for certificate verification
 
@@ -22,14 +37,13 @@ def download_pdf_file(papers: list) -> bool:
             if paper.get("openAccessPdf"):
                 response = session.get(paper["openAccessPdf"]["url"], stream=True)
 
-                # isolate PDF filename from URL
+                # Isolate PDF filename from URL
                 pdf_file_name = os.path.basename(paper["openAccessPdf"]["url"])
                 if response.status_code == 200:
-                    # Save in current working directory
-                    filepath = os.path.join(os.getcwd(), f"pdfs/{pdf_file_name}.pdf")
-                    with open(filepath, "wb") as pdf_object:
-                        pdf_object.write(response.content)
-                        print(f"{pdf_file_name} was successfully saved!")
+                    # Upload directly to S3
+                    print(f"Uploading {pdf_file_name} to S3...")
+                    s3.upload_fileobj(response.raw, bucket_name, f"{s3_folder}{search_string}{pdf_file_name}.pdf")
+                    print(f"{pdf_file_name} was successfully uploaded to S3!")
                     res.append("yes")
                 else:
                     res.append("no open access")
@@ -44,5 +58,5 @@ def download_pdf_file(papers: list) -> bool:
         json.dump(res, f)
 
 if __name__ == "__main__":
-    # URL from which pdfs to be downloaded
+    # URL from which PDFs to be downloaded
     download_pdf_file(data)
