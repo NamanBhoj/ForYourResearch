119758d8c81a4acab223641f7ae93e96baaf8f8a
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-21 17:44:25 -0600

    Add new search_results_full_text table and full_text_operations.py

--- app/schemas/search.py
+++ app/schemas/search.py
@@ -1,17 +1,9 @@
 # from ..database import Base
 from sqlalchemy import Column, String, Integer
+import database
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://doadmin:AVNS_nq_7NWCBc9D5HIhveK_@db-postgresql-tor1-74366-do-user-17707540-0.h.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-class Search(Base):
+class Search(database.Base):
     __tablename__ = "searches"
 
     search_id = Column(Integer, primary_key=True, autoincrement=True)
--- app/schemas/search_result_title_abstract.py
+++ app/schemas/search_result_title_abstract.py
@@ -1,20 +1,10 @@
 # from ..database import Base
 from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import database
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://doadmin:AVNS_nq_7NWCBc9D5HIhveK_@db-postgresql-tor1-74366-do-user-17707540-0.h.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-class SearchResult(Base):
-    __tablename__ = "search_results"
+class SearchResult(database.Base):
+    __tablename__ = "search_results_title_abstract"
 
     # Primary and foriegn keys
     search_result_id = Column(Integer, primary_key=True, autoincrement=True)
@@ -27,7 +17,6 @@ class SearchResult(Base):
     url = Column(String, nullable=True)
     paperId = Column(String, nullable=True)
     # fullText = Column(String, nullable=True)
-    tempfield = Column(String, nullable=True)
     # Relevance that is manually set by the user, manual_overall_relevance is the value of manually tagged papers
     # in our Library and PapersTable on the frontend
     manual_overall_relevance = Column(String, nullable=True)
@@ -36,7 +25,7 @@ class SearchResult(Base):
     # manual_full_text_relevance = Column(String, nullable=True)
 
     # Relevance that will be determined by the LLM
-    ai_overall_relevance = Column(String, nullable=True)
+    # ai_overall_relevance = Column(String, nullable=True)
     ai_title_relevance = Column(String, nullable=True)
     ai_abstract_relevance = Column(String, nullable=True)
     # ai_full_text_relevance = Column(String, nullable=True)
