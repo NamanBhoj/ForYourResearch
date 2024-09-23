ba668bae80a7eb207a5526921fcd68bbed4f59ee
Naman Bhoj <namanbhoj99@gmail.com>
2024-09-23 14:00:09 -0600

    changed the total to 1000 papers

--- app/routers/papers.py
+++ app/routers/papers.py
@@ -100,7 +100,7 @@ async def search(query: str):
     total_offset = 0
     limit = 100
 
-    while total_offset < 100:
+    while total_offset < 1000:
         query_params = {
             "query": parsed_query,
             "limit": limit,
