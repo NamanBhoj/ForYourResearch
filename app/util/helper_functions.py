a14784ec436a6c8654276c766e784b1228c1b7c5
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-23 15:29:03 -0600

    Added filter_papers function to remove duplicates
    The filter_papers function removes all duplicate papers before sending
    the response to the frontend because on frontend, you cant render duplicates without getting an error.
    
    To recreate:
    - remove the filter_papers function in papers.py and send the response WITH duplicates

--- app/util/helper_functions.py
+++ app/util/helper_functions.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, json
 
 
 def parse_query(query):
@@ -10,3 +10,13 @@ def parse_query(query):
 
 def is_in_production():
     return os.getenv("ENV") == "production"
+
+
+def filter_papers(papers):
+    seen_titles = set()
+    unique_papers = []
+    for paper in papers:
+        if paper["title"] not in seen_titles:
+            unique_papers.append(paper)
+            seen_titles.add(paper["title"])
+    return unique_papers
