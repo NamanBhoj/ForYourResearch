d8394cb2e56ef35bdc66ecb90f79b993f88e45f1
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2024-09-24 06:56:45 -0600

    Remove papers.py and shift the search function to search.py instead and clean up code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI
 from mangum import Mangum
-from .routers import papers, library, search
+from .routers import library, search
 from .util.helper_functions import is_in_production
 from fastapi.middleware.cors import CORSMiddleware
 from .models.search import SearchCreate
@@ -8,7 +8,6 @@ from .dependencies import get_db
 from sqlalchemy.orm import Session
 from .schemas.search import Search
 
-# from setproctitle import setproctitle
 
 # test cicd
 app = FastAPI()
@@ -29,7 +28,6 @@ if not (is_in_production()):
         allow_origins=(["*"]),
     )
 
-app.include_router(papers.router)
 app.include_router(library.router)
 app.include_router(search.router)
 
--- app/routers/search.py
+++ app/routers/search.py
@@ -1,13 +1,65 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
-from ..models.current_search_result import CurrentSearchResult
 from ..models.request_with_list import RequestObjectWithListData
 from ..dependencies import get_db
-from ..crud import current_search_result_operations, library_operations
+from ..crud import current_search_result_operations
+from ..util.helper_functions import parse_query
 from fastapi import Depends
+import requests
+import time
+from ..util.helper_functions import parse_query, filter_papers
 
 router = APIRouter()
+
+
+@router.get("/search")
+async def search(query: str):
+    url = "https://api.semanticscholar.org/graph/v1/paper/search"
+
+    parsed_query = parse_query(query)
+    print(parsed_query)
+    api_key = "Gvkbt2QFvx2QZwQBigWqJTzOa5TPS6v1kAdrpaBf"
+    headers = {"x-api-key": api_key}
+
+    total_papers = []
+    total_offset = 0
+    limit = 100
+
+    while total_offset < 100:
+        query_params = {
+            "query": parsed_query,
+            "limit": limit,
+            "fields": "title,abstract,year,openAccessPdf,isOpenAccess",
+            "offset": total_offset,
+        }
+
+        time.sleep(2)
+        response = requests.get(url, params=query_params, headers=headers)
+
+        print(response.url)
+        response_data = response.json()
+        number_of_papers = response_data["total"]
+
+        papers = response_data.get("data", [])
+        total_papers.extend(papers)
+
+        total_offset += limit
+
+        # if returned array has less than 100 papers, it means that there wont be anymore papers in the next array because
+        # the maximum limit is 100
+        if len(papers) < limit:
+            break
+    for paper in total_papers:
+        paper["Relevance"] = "Untagged"
+
+    # total_papers is the list containing all the papers
+    total_papers = filter_papers(total_papers)
+    response_object = {
+        "papers": total_papers,
+        "number_of_papers": number_of_papers,
+    }
+
+    return response_object
 
 
 @router.post("/saveCurrentSearchResults")
